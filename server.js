0c98d9d5a85608f7bceed6498a7c1bff8dabcf15
asima <asimaabdikarimoova6364@gmail.com>
2026-01-21 09:37:05 +0500

    ver1

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/myapp', {})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/blogs', blogRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
